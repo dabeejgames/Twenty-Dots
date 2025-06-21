a9c3fa610c924664735a4885ef17eb6424bc001e
dabeejgames <dabeejgames@gmail.com>
2025-06-21 13:42:51 -0500

    Describe your changes here

--- src/components/Hand.jsx
+++ src/components/Hand.jsx
@@ -1,8 +1,22 @@
 import React from "react";
 import Card from "./Card";
+
 export default function Hand({ cards, onCardClick }) {
   return (
-    <div className="hand">
+    <div
+      className="hand"
+      style={{
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "flex-start",
+        gap: 7,
+        marginBottom: 0,
+        marginTop: 2,
+        overflowX: "auto",
+        maxWidth: "100vw",
+        padding: "0 5px",
+      }}
+    >
       {cards.map((card, idx) => (
         <Card
           key={card.id}
